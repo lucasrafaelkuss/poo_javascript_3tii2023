b2bf6d42f36d38acc6acd5071c9ac7f02fae342a
lucasrafaelkuss <126463541+lucasrafaelkuss@users.noreply.github.com>
2023-02-27 07:45:33 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,21 @@
+//CRIANDO A CLASSE/OBJETO PESSOA COM
+//SEUS ATRIBUTOS NOME E SOBRENOME.
+//OBS: O OBJETO PODE TER UMA OU MILHARES DE
+//ATRIBUTOS DEPENDENDO DA NECESSIDADE DA APLICAÇÃO.
+function Pessoa (nome, sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    Object.freeze(this);
+
+
+}
+
+//INSTANCIANDO O OBJETO CRIADO
+const p1 = new Pessoa('Thiago', 'Forteski');
+const p1 = new Pessoa('João', 'Lima');
+const p1 = new Pessoa('Helena', 'Froggel');
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
